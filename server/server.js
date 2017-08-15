de80ba636281bf7b09a2a1ed703e8dce4c7c4afa
Soumik Dasgupta <soumikdasgupta1@gmail.com>
2017-08-15 17:53:49 +0530

    heroku fix final

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const path = require('path');
 const publicPath = path.join(__dirname, '/../public');
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const express = require('express'); // webserver
 
 var app = express();
 app.use(express.static(publicPath));
 
-app.get('/',function(req,res){
-      res.sendFile(publicPath + "/index.html");
-});
+// app.get('/',function(req,res){
+//       res.sendFile(publicPath + "/index.html");
+// });
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
