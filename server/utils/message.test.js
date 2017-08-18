ee4aaa3d6c29fdd52fd1c9d914aeebc652d11502
Soumik Dasgupta <soumikdasgupta1@gmail.com>
2017-08-18 19:40:59 +0530

    Add geo location support

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message'); // using ES6 destructuring
+var {generateMessage, generateLocationMessage} = require('./message'); // using ES6 destructuring
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => { // no need to provide done since its not asynchronus function
@@ -10,5 +10,18 @@ describe('generateMessage', () => {
 
     expect(message.createdAt).toBeA('number');
     expect(message).toInclude({from, text});
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate location correct message object', () => { // no need to provide done since its not asynchronus function
+    var from = 'Deb';
+    var latitude = '15';
+    var longitude = '19';
+    var url = 'https://www.google.com/maps?q=15,19'
+    var message = generateLocationMessage(from, latitude, longitude);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, url});
   });
 });
